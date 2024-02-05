1ea6065d42c54d8cf282a7b5d8bd5da98cb20205
jpalacio <jaypalacio95@gmail.com>
2024-02-05 00:21:38 -0500

    edited layout and added ThemeToggler.js to components

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,9 +15,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider>
+        <ThemeProvider attribute="class" defaultTheme='light'>
           <Header />
           {children}
           <Footer />
