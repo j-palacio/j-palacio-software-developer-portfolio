05a8ca9036fe296b0f988c5e5a9577d8ce51dda5
jpalacio <jaypalacio95@gmail.com>
2024-03-05 01:24:45 -0500

    half way thorugh the hero section of the project

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -16,17 +16,31 @@ import Socials from "./Socials";
 
 const Hero = () => {
   return (
-    <section>
+    <section
+      className="py-12 xl:py-24 h-[84vh] xl:pt-28 bg-hero bg-no-repeat
+    bg-bottom bg-cover dark:bg-none"
+    >
       <div className="container mx auto">
-        <div>
-          {/* text */}  
-          <div>text</div>
-          {/* image */}  
+        <div className="flex justify-between gap-x-8">
+          {/* text */}
+          <div>
+            <div className='text-sm uppercase font-semibold mb-4 text-primary
+            tracking-[4px]'>Web Developer</div>
+            <h1 className='h1'>Hello, my name is Juan Palacio</h1>
+            <p>
+              Brief description with insights into myself, my vocational
+              journey, and what I engae in profesionally.
+            </p>
+          </div>
+          {/* image */}
           <div>image</div>
         </div>
         {/* icon */}
-        <div className='hidden md:flex'>
-            <RiArrowDownSLine className='text-3xl text-primary' />
+        <div
+          className="hidden md:flex left-2/4 bottom-44 xl:bottom-12 
+        animate-bounce"
+        >
+          <RiArrowDownSLine className="text-3xl text-primary" />
         </div>
       </div>
     </section>
